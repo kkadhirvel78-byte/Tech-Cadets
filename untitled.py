90f3f9f04715d90bcbb039f27204d39fa2cf459b
kkadhirvel78-byte <kkadhirvel78@jnn.edu.in>
2025-09-02 12:18:09 +0530

    Add files via upload

---
+++ untitled.py
@@ -0,0 +1,10 @@
+a=str(input("enter the coustmer name:"))
+b=input("enter the code that he/she have purchased:")
+qua=int(input("enter the quantity that the customer had bought:")) 
+if(b=='q'):
+    total=20*qua
+elif(b=='w'):
+    total=30*qua
+print("total amount is",total)
+
+
